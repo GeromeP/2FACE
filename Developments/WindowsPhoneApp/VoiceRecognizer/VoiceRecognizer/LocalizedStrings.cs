479f8fd57a9ad2995c47c37e80b913b18ccbffe2
BrunoF <fbruno@hotmail.fr>
2014-01-15 15:51:18 +0100

    WPA Voice recognition

---
+++ Developments/WindowsPhoneApp/VoiceRecognizer/VoiceRecognizer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using VoiceRecognizer.Resources;
+
+namespace VoiceRecognizer
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
